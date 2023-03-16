d355741ff739353ea6eeb8d1ed51e24e1fa1eb9b
MrNetCode <deferreandrea@gmail.com>
2023-03-16 20:11:28 +0100

    d

--- functions/DB_Connection.ts
+++ functions/DB_Connection.ts
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 
 // Create a MySQL database connection using environment variables for configuration
 export const connection = mysql.createConnection({
-    host: process.env.DB_HOST || "localhost", // The host name of the database server
+    host: process.env.DB_HOST || "127.0.0.1", // The host name of the database server
     user: process.env.DB_USER || "root", // The database user's username
     password: process.env.DB_PASS, // The database user's password
     database: process.env.DB_DEFAULT_SCHEMA || "test", // The name of the default schema/database
