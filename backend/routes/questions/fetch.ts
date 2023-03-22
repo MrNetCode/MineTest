d7c9494c847435807054abb84693c970c1f5848e
MrNetCode <deferreandrea@gmail.com>
2023-03-22 12:12:58 +0100

    Update Test Update Question create to fix bug related to true-false Added Question fetch

---
+++ backend/routes/questions/fetch.ts
@@ -0,0 +1,45 @@
+import crypto from "crypto";
+import dotenv from "dotenv";
+import express from "express";
+import { connection } from "../../functions/DB_Connection";
+import cors from "cors";
+import multer from "multer";
+
+dotenv.config();
+const router = express.Router();
+
+console.log("Loaded Question Create Endpoint")
+
+// enable Cross-Origin Resource Sharing
+router.use(cors());
+
+// initialize multer for file uploads
+const upload = multer();
+
+// handle POST requests to create new questions
+router.post("/", upload.none(), async (request, response) => {
+    try {
+        if(!request.body){
+            return response.status(400).send({message :"Bad request"})
+        }
+      const { testId } = request.body;
+  if(!testId){
+    return response.status(401).send({message :"Missing testId"})
+  }
+      const result: any = await (await connection).query(`
+        SELECT q.id, q.type, q.text, mc.choice1, mc.choice2, mc.choice3, mc.choice4, mc.choice5, mc.correct AS correctAnswer, tf.answer AS correctAnswer
+        FROM questions q
+        LEFT JOIN multi_choice mc ON q.id = mc.id
+        LEFT JOIN true_false tf ON q.id = tf.id
+        WHERE q.test = ?
+      `, [testId]);
+  
+      const questions = result[0];
+      return response.status(200).send({ "question": questions });
+    } catch (error) {
+      console.log(error);
+      return response.status(500).send({ error: "Internal server error" });
+    }
+  });
+  
+  export default router
--- backend/tests/user.test.tsx
+++ backend/tests/user.test.tsx
@@ -3,6 +3,58 @@ import dotenv from "dotenv";
 dotenv.config();
 
 describe("Register API Test", () => {
+
+  test("Register with no FormData", async () => {
+    const formData: any = new FormData();
+
+    const response = await fetch("http://localhost:5000/api/user/register", {
+      method: "POST",
+      body: formData,
+    });
+    const data = await response.json();
+    expect(response.status).toBe(401);
+  });
+
+  test("Register with no username", async () => {
+    const formData: any = new FormData();
+    formData.append("password", process.env.TEST_PASSWORD || "password");
+    formData.append("code", "00000000");
+
+    const response = await fetch("http://localhost:5000/api/user/register", {
+      method: "POST",
+      body: formData,
+    });
+    const data = await response.json();
+    expect(response.status).toBe(401);
+  });
+
+  test("Register with no password", async () => {
+    const formData: any = new FormData();
+    formData.append("username", process.env.TEST_USERNAME || "Testuser");
+    formData.append("code", "00000000");
+
+    const response = await fetch("http://localhost:5000/api/user/register", {
+      method: "POST",
+      body: formData,
+    });
+    const data = await response.json();
+    expect(response.status).toBe(401);
+  });
+
+  test("Register with no code", async () => {
+    const formData: any = new FormData();
+    formData.append("username", process.env.TEST_USERNAME || "Testuser");
+    formData.append("password", process.env.TEST_PASSWORD || "password");
+    
+
+    const response = await fetch("http://localhost:5000/api/user/register", {
+      method: "POST",
+      body: formData,
+    });
+    const data = await response.json();
+    expect(response.status).toBe(401);
+  });
+  
   test("Register with invalid totp code", async () => {
     const formData: any = new FormData();
     formData.append("username", process.env.TEST_USERNAME || "TestUser");
@@ -16,21 +68,6 @@ describe("Register API Test", () => {
     const data = await response.json();
     expect(response.status).toBe(401);
     expect(data.message).toEqual("Invalid TOTP code");
-  });
-
-  test("Register with missing username, code or password", async () => {
-    const formData: any = new FormData();
-    formData.append("username", "");
-    formData.append("password", process.env.TEST_PASSWORD || "password");
-    formData.append("code", "123456");
-
-    const response = await fetch("http://localhost:5000/api/user/register", {
-      method: "POST",
-      body: formData,
-    });
-    const data = await response.json();
-    expect(response.status).toBe(401);
-    expect(data.message).toEqual("missing username, code or password");
   });
 
   test("Register with illegal username", async () => {
