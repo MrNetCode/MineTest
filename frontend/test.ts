abc7a661c38e5ad4c9462bab53d0959841e1c2d6
MrNetCode <119935877+MrNetCode@users.noreply.github.com>
2023-03-24 13:08:21 +0100

    Frontend-field-checks (#10)
    * Added field checks
    
    * Fixed Add styling on "test.html" #7

--- frontend/test.ts
+++ frontend/test.ts
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", async function () {
 createQuestionForm.reset();
 
 
+// Add an event listener to each radial input element
+
+
   if (!localStorage.getItem("token")) {
     window.location.href = "./login.html";
   }
@@ -125,7 +128,46 @@ createQuestionForm.reset();
   } catch (error) {
     console.log(error);
   }
+
+  const questionTextInput:any = document.getElementById("question-text-input");
+  const correctAnswerInputTf:any = document.getElementById("correct-answer-input-tf");
+  const correctAnswerInputMc = document.getElementById("correct-answer-input-mc");
+  const choice1Input = document.getElementById("choice1-input");
+  const choice2Input = document.getElementById("choice2-input");
+  
+  createQuestionForm.addEventListener("change", () => {
+    let isValid = true;
+  
+    // Check if question text has a value
+    if (questionTextInput.value === "") {
+      isValid = false;
+    }
+  
+    // Check if question type has a value
+    if (questionTypeSelect.value === "") {
+      isValid = false;
+    } else if (questionTypeSelect.value === "true-false") {
+      // Check if true/false question has a valid answer
+      if (correctAnswerInputTf.value === "") {
+        isValid = false;
+      }
+    } else if (questionTypeSelect.value === "multi") {
+      // Check if multiple choice question has a valid answer and at least two choices
+      if (correctAnswerInputMc.value === "") {
+        isValid = false;
+      }
+      if (choice1Input.value === "" || choice2Input.value === "") {
+        isValid = false;
+      }
+    }
+  
+    // Enable/disable submit button based on validity
+    const submitButton = createQuestionForm.querySelector("button[type='submit']");
+    console.log(isValid)
+    submitButton.disabled = !isValid;
+  });
 });
+
 
 // Get the elements
 const questionTypeSelect: any = document.getElementById('question-type-select');
